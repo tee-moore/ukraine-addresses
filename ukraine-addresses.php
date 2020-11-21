782d8b718acf8418081b3368b9e6448c1785b5b3
tee-moore <2teemoore@gmail.com>
2020-11-21 16:10:25 +0200

    Added composer and autoload

--- ukraine-addresses.php
+++ ukraine-addresses.php
@@ -34,6 +34,10 @@ if (!defined('WPINC')) {
     die;
 }
 
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
 /**
  * Currently plugin version.
  */
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8db1b41cf70fa9d1a3f6333f86e9dffb932ee586',
+    'name' => 'teemoore/ukraine-addresses',
+  ),
+  'versions' => 
+  array (
+    'teemoore/ukraine-addresses' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8db1b41cf70fa9d1a3f6333f86e9dffb932ee586',
+    ),
+  ),
+);
